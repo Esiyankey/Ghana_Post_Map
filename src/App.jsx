af237dddab25197b7491eb3ee7a5108a619ac3ad
Esi Yankey <graceyan244@gmail.com>
2023-05-04 18:30:52 +0000

    fix: fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import Navbar from './Navbar'
 import MainContainer from './MainContainer';
 import FooterContainer from './FooterContainer';
+import './App.css';
 
 
 
 export default function App() {
   return (
-    <div className='flex flex-col h-screen'>
+    <div className='flex flex-col h-screen .MainApp'>
       <Navbar/>
       <MainContainer/>
       <FooterContainer/>
