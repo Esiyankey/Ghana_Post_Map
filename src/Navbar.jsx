c8e4071224213759ba0d8ea05d4ae0b2c2c5d1c5
Esi Yankey <graceyan244@gmail.com>
2023-05-04 18:12:47 +0000

    fix: fix some bugs

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,10 +7,10 @@ import { FaGlobe } from "react-icons/fa";
 
 export default function Navbar() {
     return (
-                <div className="bg-blue-700  h-14  px-6 flex items-center justify-between text-center " >
+                <div className="bg-blue-700  h-14  px-6 flex items-center justify-between text-center Navbar" >
                     <div className='w-52  flex justify-between items-center '>
                         <img src="logo.png" alt="logo-here" className='w-28 object-cover' />
-                        <button className= " h-9 w-9 flex justify-center items-center hover:bg-blue-600"><FaBars className=' text-gray-300'/></button>
+                        <button className= " btn h-9 w-9 flex justify-center items-center hover:bg-blue-600"><FaBars className=' text-gray-300'/></button>
                     </div>
                     <div>
                         <button className="  w-36 h-8 bg-green-600 border-black flex justify-between px-2 py-5 text-center text-white items-center " >
