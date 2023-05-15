ee38e3346370a78c46a602173a5fd33cccf083c9
Esi Yankey <graceyan244@gmail.com>
2023-05-15 19:14:24 +0000

    fix : fix a bug

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -23,9 +23,9 @@ export default function MainContainer() {
   return (
     <>
       <div className="  flex overflow-y-auto">
-        <div className="fixed overflow-y-auto sm:static  sm:bg-white  w-[100%]  sm:w-[400px] ">
+        <div className=" overflow-y-auto sm:static fixed  sm:bg-white    sm:w-[400px] ">
           <Search onLocationChange={handleLocationChange}   onRegionChange={handleRegionChange} onStreetChange={handleStreetChange}/>
-          <h5 className="  py-4 font-light invisible sm:visible   ">
+          <h5 className="  py-4 font-light hidden sm:block   ">
             Address Info
           </h5>
 
@@ -35,7 +35,7 @@ export default function MainContainer() {
         </div>
 
         <LoadScript googleMapsApiKey="AIzaSyBwfcFbeO3aEul99U-XPn0aN8fbH2NUjGw">
-          <div className="w-[950px] h-[520px] sm:h-[100%] relative top-16 sm:top-0 map-container  -z-10 sm:z-0 sm:w-full">
+          <div className="w-[100%] h-[520px] sm:h-[100%] relative top-16 sm:top-0 map-container  -z-10 sm:z-0 sm:w-full">
             <GoogleMap zoom={15} center={location}>
                 <Marker position={{ lat: location.lat, lng: location.lng }}/>
                 <Marker position={{ lat: location.lat, lng: location.lng }}/>
